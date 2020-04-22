e523ca30ca970f106e5193c244fdc4acd7243f2f
BobSappp <bb35319048@ms.nagasaki-u.ac.jp>
2020-04-22 23:00:53 +0900

    k01

--- k01/k01.c
+++ k01/k01.c
@@ -3,15 +3,22 @@
 #include <string.h>
 #include <math.h>
 
-extern double ave_online(double val,double ave)
-extern double var_online()
+extern double ave_online(double val,double ave,int i){
+    return(ave*(i-1)) + val/i)
+}
+extern double var_online(double val,double ave, double square_ave, int i){
+    return(((i-1)*square_ave/i) + pow(val,2)/i-pow((i-1)*ave/i+(val/i)))
+}
 
 int main(void)
 {
-    double val;
+    double val,ave,var,square_ave;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    double average;
+    int i;
+
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -23,10 +30,12 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
+i=0
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
+        i++;
+        var = var_online(val,ave,square_ave,i);
+        ave = ave_online(square_ave,pow(val,2),i);
 
     
 
@@ -39,9 +48,8 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-
-    return 0;
-
+printf("ave=%1f\n",ave);
+printf("var=%1F\n",var);
 
 }
 
