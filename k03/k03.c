cc7fb7b083968c15ea678a0e090fe954eefe0b62
BobSappp <bb35319048@ms.nagasaki-u.ac.jp>
2020-08-04 13:47:11 +0900

    修正しました

--- k03/k03.c
+++ k03/k03.c
@@ -8,7 +8,6 @@
 #include <time.h>
 #define RAND_SEED ((unsigned) time(NULL))
 #endif
-#define M_PI 3.14
 
 extern double r_unif(void);
 extern double r_stdnorm(void);
@@ -18,18 +17,25 @@ int main(int argc, char* argv[])
     int num_dummy;
     double mu;
     double sigma;
-    int i;
+    int i,n;
     double dummy;
 
 
+    
+    
 
 
 
     printf("============================================\n");
+    sscanf(argv[1],"%lf",&mu);
     printf("template mean: %4.1lf\n",mu);
+    sscanf(argv[2],"%lf",&sigma);
     printf("template standard deviation: %3.2lf\n",sigma);
+    sscanf(argv[3],"%d",&num_dummy);
     printf("Num of dummy data: %d\n",num_dummy);
     printf("============================================\n");
+        
+    
 
     srand(RAND_SEED);
     for( i=1;i<num_dummy;i++){
